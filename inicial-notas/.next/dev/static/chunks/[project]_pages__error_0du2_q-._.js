3ffd9f0d8aac34ab4e568cc88cc77eb941c2b67e
Marcelo Ochoa <202320991@urp.edu.pe>
2026-08-06 16:19:34 -0500

    feat: proyecto limpio con filtros por bimestre

--- inicial-notas/.next/dev/static/chunks/[project]_pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/1fz1_next_dist_compiled_03nk524._.js","static/chunks/1fz1_next_dist_shared_lib_0a6suzm._.js","static/chunks/1fz1_next_dist_client_0__rxeq._.js","static/chunks/1fz1_next_dist_0-q2pvb._.js","static/chunks/1-4i_next_dist_pages__error_139outm.js","static/chunks/[next]_entry_page-loader_ts_1aoli7m._.js","static/chunks/1fz1_react-dom_1hcypmz._.js","static/chunks/1fz1_0d7hak2._.js","static/chunks/[root-of-the-server]__02sxxph._.js"],
-    source: "entry"
-});
